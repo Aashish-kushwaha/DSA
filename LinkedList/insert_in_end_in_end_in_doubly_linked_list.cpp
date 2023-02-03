2129347ad7da06c12c04ac84578435d172a67896
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-02-03 22:46:11 +0530

    insert data in the end of ldoubly linked list

---
+++ LinkedList/insert_in_end_in_end_in_doubly_linked_list.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node *prev;
+    node(int x)
+    {
+        data=x;
+        prev=NULL;
+        next=NULL;
+    }
+};
+node *insertr(node *h,int x)
+{
+    node *n=new node(x);
+    if(h==NULL)
+    {
+        return n;
+    };
+    node *curr=h;
+    while(curr->next!=NULL)
+    {
+        curr=curr->next;
+    }
+    curr->next=n;
+    n->prev=curr;
+    return h;
+
+}
+
+void printr(node *h)
+{
+    node *c=h;
+    while(c!=NULL)
+    {
+        cout<<c->data<<" ";
+        c=c->next;
+    }
+}
+int main()
+{
+    node *head=NULL;
+    head=insertr(head,10);
+    head=insertr(head,20);
+    head=insertr(head,30);
+    head=insertr(head,40);
+    printr(head);
+    
+}
