5083e614ba00293f961f099ec28ae1ad2d9544d8
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-02-23 00:15:36 +0530

    implementation of tree

---
+++ Tree/implementation_of_binary_tree.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct treenode{
+    int data;
+    treenode *left;
+    treenode *right;
+
+    treenode(int x)
+    {
+        data=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+treenode *insertion(treenode *root,int n)
+{
+   if(root==NULL)
+   {
+    root=new treenode(n);
+    return root;
+   }
+
+   queue<treenode*> q;
+   q.push(root);
+   while(!q.empty())
+   {
+    treenode *curr=q.front();
+    q.pop();
+
+    if(curr->left==NULL){
+    curr->left=new treenode(n);
+    return root;
+    }else{
+        q.push(curr->left);
+
+    }
+
+    if(curr->right==NULL)
+    {
+        curr->right=new treenode(n);
+        return root;
+    }else{
+        q.push(curr->right);
+    }
+
+   }
+   return root;
+}
+void inorder(treenode *root)
+{
+    if(root==NULL)
+    return;
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(treenode *root)
+{
+    if(root==NULL)
+    return;
+
+    cout<<root->data<<" ";
+    inorder(root->left);
+
+    inorder(root->right);
+}
+
+void postorder(treenode *root)
+{
+    if(root==NULL)
+    return;
+
+    inorder(root->left);
+    
+    inorder(root->right);
+    cout<<root->data<<" ";
+}
+int main()
+{
+    treenode *root=NULL;
+   root= insertion(root,2);
+   root= insertion(root,7);
+   root= insertion(root,56);
+   root= insertion(root,6);
+    root=insertion(root,5);
+    root=insertion(root,4);
+    root=insertion(root,3);
+    cout<<"inorder:";
+    inorder(root);
+    cout<<endl;
+    cout<<"preorder:";
+    preorder(root);
+    cout<<endl;
+    cout<<"postorder:";
+    postorder(root);
+}
