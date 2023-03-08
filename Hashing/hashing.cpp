073044b460dabe099c301ee1ec787fb068d27658
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-03-08 20:13:13 +0530

    implementation of chaining

--- Hashing/hashing.cpp
+++ Hashing/hashing.cpp
@@ -27,4 +27,31 @@ application of hashing
 * should generate values from 0 to n-1
 * should be fast
 * should uniformly distribute large keys into hash table slots.
+
+
+example of hash function
+1. for integer :
+                h(large key)= large key%m
+2. for string:
+              str[]="abcd"
+            (str[0]*x^0+ str[1]*x^1+ str[2]*x^2+ str[3]*x^3)/m
+3. universal hashing   
+
+collision handling
+
+in hashing collision is bound to happen 
+-> if we know in advance ,then we can do perfect hashing.
+-> if we do not know keys, then we use one of the following :
+1.chaining
+2.open addressing 
+|->linear probing
+|->Quadratic probing
+|->double hashing
+
+CHAINING 
+data structure used for implementing chaining
+1. linked list: insert:O(n) search:O(n) delete:O(n) disadvantage: not cache friendlyness
+2. dynamic sized array(vectors in c++, arraylist in java and list in python): insert:O(n)for sorted list  search:O(n) delete:O(n) but it has one advantage that it is cache friendlyness  
+3. self balancing BST(AVL trees , red black trees): insert:O(log n) search:O(log n) delete:O(n) 
+
 */
