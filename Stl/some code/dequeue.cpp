88f00f9c01fef7868ea75a214d9fe6bcdd833728
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-03-23 20:13:05 +0530

    deque data structure

--- Stl/some code/dequeue.cpp
+++ Stl/some code/dequeue.cpp
@@ -1,10 +1,11 @@
-// dubly ended queue:we have option to insertor delete  either on start or last of the queue and is faster then vector
-//for insertion and deletion,no contigious memory
+// double-ended queue:we have option to insert or delete  either on start or last of the queue and is faster then vector
+//for insertion and deletion,no contigious memory.random access
 #include<iostream>
 #include<deque>
 using namespace std;
 int main()
 {
+    //initialize
     deque <int> dq;
     int n;
     cin>>n;
@@ -14,17 +15,37 @@ int main()
         cin>>key;
         dq.push_front(key);
     }
+    // front element
     cout<<dq.front();
     cout<<endl;
-    
+    // last element
+    cout<<dq.back();
+    cout<<endl;
+
+    //remove last element
     dq.pop_back();
+
+    // remove first element
     dq.pop_front();
+
+    //for loop
     for(auto it=dq.begin();it!=dq.end();it++)
     {
         cout<<*it<<" ";
     }
     cout<<endl;
+
+    // for each loop
+    for(auto it:dq)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+    // size of deque
     cout<<dq.size();
+
+    // if deque is empty
     if(dq.empty())
     {
         cout<<"empty";
@@ -33,7 +54,17 @@ int main()
         cout<<"not empty";
     }
     cout<<endl;
-    cout<<dq.at(3);
+
+    // access element
+    cout<<dq.at(2);
+
+
+    // insert element
+    dq.insert(dq.begin(),34);
+
+    cout<<dq.front();
+
+    
 
 }
 
